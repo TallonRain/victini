bad1733dd56ed7b05f3a457d32c97dbcf7e40fb7
Kyle Worthington <tallonrain@tallonra.in>
2024-12-04 20:15:19 -0800

    Victini 1.0 RC; V-Wheel implemented

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from discord.ext import commands, tasks
 from dotenv import load_dotenv
 import fixup
 import datetime
+from zoneinfo import ZoneInfo
 import random
 
 # Load environment variables from the .env file (if present) and declare the variable
@@ -15,32 +16,41 @@ intents.message_content = True
 intents.members = True
 intents.presences = True
 
-bot = discord.Client(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Set trigger time: Noon PST -> 20:00 UTC (adjust as needed)
-utc = datetime.timezone.utc
-trigger_time = datetime.time(hour=20, minute=00, tzinfo=utc)
+pacific = ZoneInfo("America/Los_Angeles")
+trigger_time = datetime.time(hour=12, minute=00, tzinfo=pacific)
+v_wheel_channel = None
 
-class Victini(commands.Bot):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+# list of server roles
+type_roles = {}
+const_types = ["Normal", "Fire", "Water", "Grass", "Electric", "Ice", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dragon", "Dark", "Steel", "Fairy"]
 
-    async def setup_hook(self) -> None:
-        self.spin_wheel.start()
+async def setup_hook(self) -> None:
+    self.spin_wheel.start()
 
-    @tasks.loop(time=trigger_time)
-    async def spin_wheel(self):
-        print("Made it to wheel-spinning!")
-
-    @spin_wheel.before_loop
-    async def pre_check(self):
-        await self.wait_until_ready()
+@tasks.loop(time=trigger_time)
+async def spin_wheel():
+    print("Activating the V-Wheel!!")
+    # need to iterate through the roles so that the previous winner can be dehoisted automatically
+    for role in bot.guilds[0].roles:
+        if role.name in const_types:
+            await role.edit(hoist=False)
+    await v_wheel_channel.send("It's time to spin the V-Wheeeeeeeeeeeeeeeel!")
+    vwheel_type = random.choice(const_types) # spiiiiiinnnnnnn
+    await v_wheel_channel.send(f"Today's V-Wave type is... {vwheel_type}! Go say hi to the lucky {vwheel_type} types!")
+    role_id = type_roles.get(vwheel_type) # fetch the roleID of the winner
+    role = bot.guilds[0].get_role(role_id) # format it as a Discord object so it can be manipulated
+    if role is not None:
+        print("Fetched the role ID successfully")
+        await role.edit(hoist=not role.hoist) # inverts the current setting
+    else:
+        print("Error fetching server roles, cannot hoist the v-wheel winner")
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} has logged into Discord!')
-    v_wheel_channel = await bot.fetch_channel(1313952056481939586)
-
 
 @bot.event
 async def on_message(message):
@@ -68,12 +78,16 @@ async def on_message(message):
 
 @bot.event
 async def on_ready():
+    global v_wheel_channel
     print(f"{bot.user} has logged into Discord! Setting up...")
+    spin_wheel.start()
     v_wheel_channel = await bot.fetch_channel(1313952056481939586) # channel id, as an int, goes here
     print(f"Found the {v_wheel_channel} channel, saying hi!")
     # await v_wheel_channel.send("Tadaaaa~! It's me, Victini~!")
+    for role in bot.guilds[0].roles:
+        type_roles[role.name] = role.id # fetch the server's roles and put them in a dictionary with the name and ID
+        print(f"Role Name: {role.name} | Role ID: {role.id}")
 
 # fixup addresses a fatal SSL & authentication bug in Discord.py
 fixup.run(bot, DISCORD_TOKEN)
-bot.start(DISCORD_TOKEN)
 
